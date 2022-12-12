e1df6a13784764b1bb639e2d1ab0a76bf8a6004a
Claire Villaret <clairevillaret@gmail.com>
2022-12-12 09:05:52 +0800

    converted list to button

--- lib/list_hospital.dart
+++ lib/list_hospital.dart
@@ -31,61 +31,92 @@ class _ListHospitalState extends State<ListHospital>{
           children: [
             Row(
               children: [
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: NetworkImage(hospital['pic_url']),
-                  backgroundColor: Colors.transparent,
-                ),
-                const SizedBox(width: 8.0),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      hospital["name"],
-                      style: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        height: 2.0,
+                RawMaterialButton(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage: NetworkImage(hospital['pic_url']),
+                        backgroundColor: Colors.transparent,
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text('Ambulance: ',style: TextStyle(fontSize: 16.0),),
-                        Text(
-                          hospital['ambulance'],
-                          style: const TextStyle(fontSize: 16.0),)
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Text('ER beds: ',style: TextStyle(fontSize: 16.0),),
-                        Text(
-                          hospital["er_bed"].toString(),
-                          style: const TextStyle(fontSize: 16.0),)
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Text('Private rooms: ',style: TextStyle(fontSize: 16.0),),
-                        Text(
-                          hospital["private_room"].toString(),
-                          style: const TextStyle(fontSize: 16.0),)
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Text('Wards: ',style: TextStyle(fontSize: 16.0),),
-                        Text(
-                          hospital["ward"].toString(),
-                          style: const TextStyle(fontSize: 16.0),)
-                      ],
-                    ),
-                  ],
+                      const SizedBox(width: 8.0),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            hospital["name"],
+                            style: const TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              height: 2.0,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('Ambulance: ',style: TextStyle(fontSize: 16.0),),
+                              Text(
+                                hospital['ambulance'],
+                                style: const TextStyle(fontSize: 16.0),)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text('ER beds: ',style: TextStyle(fontSize: 16.0),),
+                              Text(
+                                hospital["er_bed"].toString(),
+                                style: const TextStyle(fontSize: 16.0),)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text('Private rooms: ',style: TextStyle(fontSize: 16.0),),
+                              Text(
+                                hospital["private_room"].toString(),
+                                style: const TextStyle(fontSize: 16.0),)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              const Text('Wards: ',style: TextStyle(fontSize: 16.0),),
+                              Text(
+                                hospital["ward"].toString(),
+                                style: const TextStyle(fontSize: 16.0),)
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  onPressed: (){
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: const Text("Select this hospital",
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        actions: [
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text("YES",
+                              style: TextStyle(fontSize: 25.0),),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("NO",
+                              style: TextStyle(fontSize: 25.0),),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
-                //const Divider(height: 5.0, color: Colors.black38,)
               ],
             ),
           ],
@@ -137,129 +168,128 @@ class _ListHospitalState extends State<ListHospital>{
           ),
         ),
         body: SafeArea(
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        const Text("Filter by: ",
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      const Text("Filter by: ",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black54,
+                        ),),
+                      TextButton(
+                        onPressed: (){},
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: const BorderSide(color: Color(0xFFba181b))))
+
+                        ),
+                        child: const Text("Nearby",
                           style: TextStyle(
+                            color: Color(0xFFba181b),
                             fontSize: 18.0,
-                            color: Colors.black54,
                           ),),
-                          TextButton(
-                              onPressed: (){},
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18.0),
-                                  side: const BorderSide(color: Color(0xFFba181b))))
+                      ),
+                      TextButton(
+                        onPressed: (){
 
-                            ),
-                              child: const Text("Nearby",
-                              style: TextStyle(
-                                color: Color(0xFFba181b),
-                                fontSize: 18.0,
-                              ),),
-                          ),
-                        TextButton(
-                          onPressed: (){
+                        },
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: const BorderSide(color: Color(0xFFba181b))))
 
-                          },
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18.0),
-                                      side: const BorderSide(color: Color(0xFFba181b))))
-
-                          ),
-                          child: const Text("Public",
-                            style: TextStyle(
-                              color: Color(0xFFba181b),
-                              fontSize: 18.0,
-                            ),),
                         ),
-                        TextButton(
-                          onPressed: (){},
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18.0),
-                                      side: const BorderSide(color: Color(0xFFba181b))))
+                        child: const Text("Public",
+                          style: TextStyle(
+                            color: Color(0xFFba181b),
+                            fontSize: 18.0,
+                          ),),
+                      ),
+                      TextButton(
+                        onPressed: (){},
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: const BorderSide(color: Color(0xFFba181b))))
 
-                          ),
-                          child: const Text("Private",
-                            style: TextStyle(
-                              color: Color(0xFFba181b),
-                              fontSize: 18.0,
-                            ),),
                         ),
-                        TextButton(
-                          onPressed: (){},
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18.0),
-                                      side: const BorderSide(color: Color(0xFFba181b))))
+                        child: const Text("Private",
+                          style: TextStyle(
+                            color: Color(0xFFba181b),
+                            fontSize: 18.0,
+                          ),),
+                      ),
+                      TextButton(
+                        onPressed: (){},
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: const BorderSide(color: Color(0xFFba181b))))
 
-                          ),
-                          child: const Text("Availability",
-                            style: TextStyle(
-                              color: Color(0xFFba181b),
-                              fontSize: 18.0,
-                            ),),
                         ),
-                        TextButton(
-                          onPressed: (){},
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all(
-                                  RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(18.0),
-                                      side: const BorderSide(color: Color(0xFFba181b))))
+                        child: const Text("Availability",
+                          style: TextStyle(
+                            color: Color(0xFFba181b),
+                            fontSize: 18.0,
+                          ),),
+                      ),
+                      TextButton(
+                        onPressed: (){},
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: const BorderSide(color: Color(0xFFba181b))))
 
-                          ),
-                          child: const Text("Specialty",
-                            style: TextStyle(
-                              color: Color(0xFFba181b),
-                              fontSize: 18.0,
-                            ),),
                         ),
-                        ],
-                    ),
+                        child: const Text("Specialty",
+                          style: TextStyle(
+                            color: Color(0xFFba181b),
+                            fontSize: 18.0,
+                          ),),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  child: FirebaseAnimatedList(
-                    query: dbRef,
-                    itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
-                      Map hospital = snapshot.value as Map;
-                      hospital['key'] = snapshot.key;
+              ),
+              Expanded(
+                child: FirebaseAnimatedList(
+                  query: dbRef,
+                  itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
+                    Map hospital = snapshot.value as Map;
+                    hospital['key'] = snapshot.key;
 
-                      final name = snapshot.child('name').value.toString();
+                    final name = snapshot.child('name').value.toString();
 
-                      if(controller.text.isEmpty){
-                        return listItem(hospital:hospital);
-                      }
-                      else if(name.toLowerCase().contains(controller.text.toLowerCase())){
-                        return listItem(hospital: hospital);
-                      }
-                      else{
-                        return Container(
-                          color: Colors.white,
-                        );
-                      }
-                    },
-                  ),
+                    if(controller.text.isEmpty){
+                      return listItem(hospital:hospital);
+                    }
+                    else if(name.toLowerCase().contains(controller.text.toLowerCase())){
+                      return listItem(hospital: hospital);
+                    }
+                    else{
+                      return Container(
+                        color: Colors.white,
+                      );
+                    }
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
         )
     );
 
   }
 }
-
